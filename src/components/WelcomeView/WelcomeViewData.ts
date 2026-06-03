2e2150d5aac8b7ecb99b48ea968e1837248480d9
YP00 <PersonalYP00@icloud.com>
2026-06-03 11:17:21 +0300

    fix: routing issue

--- src/components/WelcomeView/WelcomeViewData.ts
+++ src/components/WelcomeView/WelcomeViewData.ts
@@ -9,6 +9,7 @@ export const p2Name = ref<string>()
 export const handleNames = async () => {
   if (p1Name.value !== undefined && p2Name.value !== undefined) {
     await router.push('/game')
+  } else {
+    await router.push('/')
   }
-  await router.push('/')
 }
